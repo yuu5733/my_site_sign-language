42e72bebb03f8f04613d0af9a5323137ea812e84
yuu5733 <you1yi1.st0ries+github@gmail.com>
2024-12-24 00:04:56 +0900

    add img setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp', // または他の適切なエントリポイント
+      config: {
+        widths: [300, 600, 900],
+        formats: ['avif', 'webp'],
+      },
+    },
+  },
 });
