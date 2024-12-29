52172b4964695a9a97984e23b5b0f9284b26b6ce
yuu5733 <you1yi1.st0ries+github@gmail.com>
2024-12-30 00:38:38 +0900

    Cardの設定

---
+++ src/components/SignLanguageCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Styes from './SignLanguageCard.module.css';
+
+interface DynamicHeadingProps {
+  level: number;
+  children: string;
+}
+
+interface SignLanguageCardProps {
+  title: string;
+  description: string;
+}
+
+const DynamicHeading: React.FC<DynamicHeadingProps> = ({ level, children }) => {
+  const HeadingTag = `h${level}`;
+  return React.createElement(HeadingTag, null, children);
+};
+
+const SignLanguageCard: React.FC<SignLanguageCardProps> = ({ title, description }) => {
+  return (
+    <div>
+      <DynamicHeading level={3} children={title}></DynamicHeading>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default SignLanguageCard;
