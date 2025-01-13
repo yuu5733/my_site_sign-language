48280fa355dd64f5a8f4fe202ff95dbbb37c3dc8
yuu5733 <you1yi1.st0ries+github@gmail.com>
2025-01-13 22:35:26 +0900

    SDK利用。エラーが出ている
    parameter is required (check serviceDomain and apiKey)

---
+++ src/library/microcms.ts
@@ -0,0 +1,30 @@
+// SDK利用準備
+import type { MicroCMSQueries, MicroCMSListContent } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+// 型定義
+export type Blog = {
+  title: string;
+  content: string;
+} & MicroCMSListContent;
+
+// APIの呼び出し
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+  return await client.getList<Blog>({ endpoint: "blogs", queries });
+};
+
+export const getBlogDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Blog>({
+    endpoint: "blogs",
+    contentId,
+    queries,
+  });
+};
